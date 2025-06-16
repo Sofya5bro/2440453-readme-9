cd9c6e9e27ee681abf31ad4779b175447e83b67f
Sofya <SofjaEsenina@yandex.ru>
2025-06-16 15:26:51 +0300

    подключаем конфигурацию к приложению

--- project/apps/user/src/app/app.module.ts
+++ project/apps/user/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReadmeUserModule } from '@project/readme-user';
 import { AuthenticationModule } from '@project/authentication';
+import { UserConfigModule } from '@project/user-config';
 
 @Module({
   imports: [
     ReadmeUserModule,
     AuthenticationModule,
+    UserConfigModule,
   ],
   controllers: [],
   providers: [],
--- project/apps/user/src/main.ts
+++ project/apps/user/src/main.ts
@@ -6,14 +6,31 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('The User Service')
+    .setDescription('User Service Api')
+    .setVersion('1.0')
+    .build();
+
+  
+  
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('application.port');
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
